0879c0903c05e0ebcfe87caa7f3c7ca5fd71d9ef
AApolito92 <alan.apolito@live.com>
2022-07-04 11:47:33 -0300

    desafio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import { Header } from './components/Header/Header';
 import MainContainer from './components/Main/MainContainer';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
